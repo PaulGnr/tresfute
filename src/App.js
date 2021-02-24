fcbe7e3282eea932c5c756f408eefcf03a813e42
PaulGnr <polo.guignier@hotmail.fr>
2021-02-24 15:56:09 +0100

    Ajout des modifications non validees

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Game from './components/game/Game'
+import Home from './components/home/Home';
+import CreateServer from './components/home/server/CreateServer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/create' component={CreateServer} />
+          <Route path='/:id' component={Game} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider, useSelector } from 'react-redux'
+import rootReducer from './store/reducers/rootReduce';
+import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore, createFirestoreInstance } from 'redux-firestore'
+import { getFirebase, ReactReduxFirebaseProvider, isLoaded } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
+import firebase from 'firebase/app'
+import Auth from './components/authentication/Auth'
+
+
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebase, fbConfig)
+  )
+);
+
+const profileSpecificProps = {
+  userProfile: 'users',
+  useFirestoreForProfile: true
+}
+
+
+const rrfProps = {
+  firebase,
+  config: profileSpecificProps,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+  presence: 'presence',
+  sessions: 'sessions'
+};
+
+function AuthIsLoaded({ children }) {
+  const auth = useSelector(state => state.firebase.auth);
+  if (!isLoaded(auth)) return <div>Loading...</div>;
+  return children
+}
+
+function AuthIsEmpty({ children }) {
+  const auth = useSelector(state => state.firebase.auth);
+  if (auth.isEmpty) return <Auth />;
+  return children
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <AuthIsLoaded>
+          <AuthIsEmpty>
+            <App />
+          </AuthIsEmpty>
+        </AuthIsLoaded>
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
